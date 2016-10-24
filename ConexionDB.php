3ac13f122bf0e72185d7aaeef47f108d8f91f4c4
fperezqu <fperezqu@cl.ibm.com>
2016-10-24 14:07:06 -0300

    Create ConexionDB.php

---
+++ ConexionDB.php
@@ -0,0 +1,28 @@
+<?php
+// Parse VCAP
+if( getenv("VCAP_SERVICES") ) {
+    $json = getenv("VCAP_SERVICES");
+}
+// No DB credentials
+else {
+    throw new Exception("No Database Information Available.", 1);
+}
+
+// Decode JSON and gather DB Info
+$services_json = json_decode($json,true);
+$bludb_config = $services_json["dashDB"][0]["credentials"];
+
+// Create DB connect string
+$conn_string = "DRIVER={IBM DB2 ODBC DRIVER};DATABASE=".
+   $bludb_config["db"].
+   ";HOSTNAME=".
+   $bludb_config["host"].
+   ";PORT=".
+   $bludb_config["port"].
+   ";PROTOCOL=TCPIP;UID=".
+   $bludb_config["username"].
+   ";PWD=".
+   $bludb_config["password"].
+   ";";
+
+?>
